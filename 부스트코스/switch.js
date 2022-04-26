0b2bb9d9ac2f9e480240be49df1be6196934ad0a
seongjin77 <kssj9056@gmail.com>
2022-04-26 22:17:43 +0900

    부스트코스 파일참조의 장점

---
+++ 부스트코스/switch.js
@@ -0,0 +1,41 @@
+
+
+
+    function alistColor(color){
+        var alist = document.querySelectorAll('a');
+        var i=0;
+        while(i<alist.length){
+
+        alist[i].style.color= color;
+        i = i+1;
+    }
+};
+/* 스위처함수 시작 */
+    function switcher1(self){
+
+    var target = document.querySelector('body');
+
+    if(self.value === 'night'){
+
+    target.style.backgroundColor='black';
+    target.style.color='white';
+    self.value= 'day';
+
+    alistColor('green');
+ 
+
+    }
+
+    else {
+
+    target.style.backgroundColor='white';
+    target.style.color='black';
+    self.value= 'night';
+    alistColor('red');
+ 
+    
+}
+
+    };
+
+
