de2d4213a1842004da6bafcee2006c8c5597974e
seongjin77 <kssj9056@gmail.com>
2022-04-26 23:46:58 +0900

    객체의 기본

--- 부스트코스/JS-object/object.js
+++ 부스트코스/JS-object/object.js
@@ -1,1 +1,13 @@
 var memberArray = ['egoing', 'graphittie', 'leezhce'];
+
+var memberObject = {
+    manager : 'egoing',
+    developer : 'graphittie',
+    designer : 'leezhce'
+};
+
+memberObject.designer = 'leezche';
+console.log('디자이너', memberObject.designer);
+console.log('디자이너', memberObject['designer']);
+delete memberObject.manager
+console.log('매니저', memberObject.manager);
