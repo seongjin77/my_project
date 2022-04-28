028aaa21c7ac3ad39401835c6c77ffea2edfdd62
seongjin77 <kssj9056@gmail.com>
2022-04-28 16:27:04 +0900

    class의 의미

---
+++ 부스트코스/JS-object/class.js
@@ -0,0 +1,16 @@
+class person{
+
+};
+
+var kim = new person();
+console.log('kim', kim);
+
+// var kim = new Person('kim',10,50);
+// var lee = new Person('lee',10,15);
+
+// kim.sum = function(){
+//     return "modified : "+ (this.first + this.second);
+// };
+
+// console.log('kim', kim.sum());
+// console.log('lee', lee.sum());
