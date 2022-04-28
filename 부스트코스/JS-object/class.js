571aa4f793123a9820627b55596026368ba8eb2c
seongjin77 <kssj9056@gmail.com>
2022-04-28 16:42:28 +0900

    constructor 함수

--- 부스트코스/JS-object/class.js
+++ 부스트코스/JS-object/class.js
@@ -1,9 +1,18 @@
 class person{
 
+    constructor (name,first,second){
+        this.name= name;
+        this.first= first;
+        this.second=second;
+        
+    };
+
 };
 
-var kim = new person();
-console.log('kim', kim);
+var kim = new person('kim',10,20);
+console.log('출력', kim);
+
+/* class 안에서 constructor 함수는 객체가 생성될때 호출된다. */
 
 // var kim = new Person('kim',10,50);
 // var lee = new Person('lee',10,15);
