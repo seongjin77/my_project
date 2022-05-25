861423fa3f9a1411a79865ee00ce3f6ba70e022e
seongjin77 <kssj9056@gmail.com>
2022-05-25 18:54:41 +0900

    update

--- 코딩테스트/main.js
+++ 코딩테스트/main.js
@@ -3,3 +3,11 @@ const input = fs.readFileSync("./코딩테스트/input.txt", "utf8");
 
 console.log(input);
 
+const TESTCASE = 5; // 상수는 보통 대물자로 표기
+
+// 한 줄에 여러 변수 선언함
+let name = 'seongjin',
+    age = 27,
+    msg = 'hello';
+
+    console.log(age);
