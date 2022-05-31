dc5ec24f3025a4ad461973288baa0b98a355e460
seongjin77 <kssj9056@gmail.com>
2022-06-01 03:07:34 +0900

    제로베이스

---
+++ 코딩테스트/고차함수.js
@@ -0,0 +1,17 @@
+/* 고차함수
+
+하나 이상의 함수를 매개변수로 취하거나 함수를 결과로 반환하는 함수.
+매개변수로 전달되는 함수는 콜백 함수
+
+임의정렬: Array.sort(callback function) 문자열로 변형되기 때문에 숫자오류, 대소문자 구분되어버림
+반복 작업: Array.forEach()
+콜백함수 결과 배열 반환: Array.map()
+조건 만족하는 하나의 값 반환: Array.find()
+조건 만족하는 값 배열로 반환: Araay.fliter()
+누적 결과 값 반환: Array.reduce()
+
+
+************필수 배열 함수 https://jae04099.tistory.com/17
+
+
+*/
