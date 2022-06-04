ddec0524e976e6b055712cba57614ca1121879e8
seongjin77 <kssj9056@gmail.com>
2022-06-04 15:50:29 +0900

    제로베이스

---
+++ 코딩테스트실전문제/JavaScript 기초 문법/JavaScript기초문법_44기본문제풀이(반복문)_두수사이숫자/기본문제풀이(반복문)_두수사이숫자_문제.js
@@ -0,0 +1,34 @@
+/*** 2. 두 수 사이 숫자 ***/
+
+/* user code */
+function answer(x, y) {
+  let result = [];
+
+  if(x<y){
+    
+    for( let i=x; i<=y; i++ ) {
+      result.push(i);
+    }
+    
+  } else {
+    for(let i=y; i<=x; i++ ) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+/* main code */
+let input = [
+  // TC: 1
+  [3, 7],
+  // TC: 2
+  [8, 3],
+  // TC: 3
+  [12, 10],
+];
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(answer(input[i][0], input[i][1]));
+}
