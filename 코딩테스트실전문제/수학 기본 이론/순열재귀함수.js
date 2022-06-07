d5da600daa1bd001984ca85c58db86f747b7f31b
seongjin77 <kssj9056@gmail.com>
2022-06-08 03:42:23 +0900

    알고리즘 해결중

--- 코딩테스트실전문제/수학 기본 이론/순열재귀함수.js
+++ 코딩테스트실전문제/수학 기본 이론/순열재귀함수.js
@@ -2,6 +2,14 @@ let input = ['a', 'b', 'c'];
 let count = 0;
 
 function permutation(arr, 인덱스시작, 인덱스끝){
-
     
+    if(인덱스시작 === 인덱스끝) {
+        count++;
+        console.log(arr);
+        return;
+    }
+    for( let i = 인덱스시작; i < arr.length; i++){
+        
+        permutation(arr, 인덱스시작 +1, 인덱스끝 );
+    }
 };
