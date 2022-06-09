6ab9ee99f1c9cf13736c451760467eb874b43c11
seongjin77 <kssj9056@gmail.com>
2022-06-10 03:11:05 +0900

    자바스크립트 딥 다이브 정리

--- deepJS study/테스트용파일/test111.js
+++ deepJS study/테스트용파일/test111.js
@@ -1,14 +1,10 @@
-
-
-
-let ggg = function(){
-
-
-
-    console.log('나는');
+const person = {
+    name: 'Lee',
+    foo(cb){
+        setTimeout(cb.bind(person), 100); // person 자리에 this도 가능. 결국 이 시기에선 this는 foo메서드를 호출한 객체 person을 가리키기때문
+    }
 };
 
-console.log(a);
-var a= 'aaa';
-
-/* 객체에서 메소드를 부를때 키값을 적지 않아도 되는 이유 */
+person.foo(function(){
+    console.log(`안녕 내 이름은 ${this.name}.`);
+});
--- deepJS study/테스트용파일/test16.js
+++ deepJS study/테스트용파일/test16.js
@@ -1,5 +1,27 @@
+function sss (num){
+    this.num = num;
+    this.함수 = function (s){
+        return s + 3* this.num;
+        
+    }
 
+}
 
+let coo = new sss(4);
 
-let party;
-console.log(party);
+console.log(coo.함수(3));
+
+//생성자 함수는 new 연산자를 꼭 써야함. 안쓰면 일반함수로 호출됨. 프로퍼티 값을 주려면 this 사용 안쓰면 작동안함.
+// new 연산자를 써서 붕어빵 기계처럼 찍어 낸 이후 변수에 담아서 사용.
+
+const bar = function 생성(num){
+    this.num = num
+}
+
+let koo = new bar(5);
+
+console.log(koo);
+
+// 함수 표현식을 쓸때는 new 연산자에 변수 식별자를 붙여줘야함. 함수이름을 붙여주는건 함수 선언문!!
+// 함수 표현식에 쓸때는 익명함수를 쓰는게 일반적이지만 이름을 붙여주면 새로만들어진 객체가 변수식별자가 아니라 함수 이름으로 설정됨.
+
