d8afe7fc7fdca14ec8b7bd1675e62be68d6060e5
seongjin77 <kssj9056@gmail.com>
2022-06-14 23:37:51 +0900

    알고리즘

--- 코딩테스트실전문제/선형 자료구조/선형자료구조_11배열문제풀이_숫자빈도수구하기/배열문제풀이_숫자빈도수구하기_문제.js
+++ 코딩테스트실전문제/선형 자료구조/선형자료구조_11배열문제풀이_숫자빈도수구하기/배열문제풀이_숫자빈도수구하기_문제.js
@@ -4,6 +4,7 @@
 function answer(s, e) {
   let result = [];
   let count =[];
+  let indexcount;
  //1. s숫자부터 1씩올려서 e까지 같아지면 멈추고 배열에 넣어줌.
   while(s<=e){
     count.push(s);
@@ -12,7 +13,11 @@ function answer(s, e) {
  //2. 만들어진 모든 배열의 숫자를 문자열로 변경시킴.
  result = count.join();
  //3. 모든 문자열중에서 0부터 9까지 각각 몇개인지 세어줌. 각각의 수만큼 결과배열에 차례대로 푸시
-  
+  for(let i=0; i<=9; i++){
+    indexcount = result.match(/0/g) // 0을 찾아서 다 배열에 indexcount 배열에 넣음
+    console.log(indexcount.length);
+    // result[i] = indexcount.length;
+  }
   return result;
 }
 
