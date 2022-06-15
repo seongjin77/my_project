1c2319e36e5174f6734ac0d4459bf7f9edb7b0c7
seongjin77 <kssj9056@gmail.com>
2022-06-15 18:20:25 +0900

    제로베이스 알고리즘

--- 코딩테스트실전문제/선형 자료구조/선형자료구조_12배열문제풀이_달팽이만들기/배열문제풀이_달팽이만들기_문제.js
+++ 코딩테스트실전문제/선형 자료구조/선형자료구조_12배열문제풀이_달팽이만들기/배열문제풀이_달팽이만들기_문제.js
@@ -8,12 +8,30 @@ function answer(length) {
     result[i] = [];
   }
   // 22.반복문 패턴
-  //1. 첫번째 열만 1번씩
+  //1. 첫번째 열만 length만큼
   //2. 두번째부터 짝수만큼 length-1개 만큼 생성
   //3. 방향바꿈
+  let direction = 1;
+  let x, y, num;
+  x=y= num =0;
+  x--;
     while(1){
-      for(let i)
+      for(let i=0; i<length; i++){
+          x += direction;
+          result[y][x] = ++num;
+      }
+
+      length--;
+      
       if (length === 0) break;
+
+      for(let j =0; j < length; j++){
+        y += direction;
+        result[y][x] = ++num;
+      }
+
+      direction *= -1;
+
     }
 
   return result;
