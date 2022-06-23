61fdc4e9b64c0b1fbde41c8ba5714657b0b1b347
seongjin77 <kssj9056@gmail.com>
2022-06-24 02:30:12 +0900

    자바스크립트 딥 다이브

--- 테스트.js
+++ 테스트.js
@@ -1,14 +1,20 @@
-let arr = [ 1, 2, 3, 4, 5]
-let result;
+// let arr = [ 1, 2, 3, 4, 5]
+// let result;
 
-result =  arr.reduce((acc, cur) => acc + cur );
+// result =  arr.reduce((acc, cur) => acc + cur );
 
-console.log(result);
+// console.log(result);
 
 
 
-const apple = function (){
-    console.log('사과');
+// const apple = function (){
+//     console.log('사과');
+// }
+
+// apple()
+
+let ff = function (x,y){
+    console.log('난');
 }
 
-apple()
+console.log(ff.length);
