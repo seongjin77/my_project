96b5c26de45c1a3a348fe93d0bd5a81a9a4941e0
seongjin77 <kssj9056@gmail.com>
2022-06-26 23:35:11 +0900

    알고리즘 연습

--- deepJS study/테스트용파일/네부테스트.js
+++ deepJS study/테스트용파일/네부테스트.js
@@ -1,17 +1,34 @@
 
 function 별(n){
 
-    let answer = '';
+    
+    for(let i =0; i< n; i++){
+        let answer = '';
+        
+        for(let j=0; j<n; j++){
+             answer += '*'
+            }
+            console.log(answer);
+        }
+        
+    
 
-    for(let i =n; i> 0; i--){
-       
-        if(n % 2 ==0) answer += '*'
-        console.log(answer);
+};// 정사각형 별 찍기 함수 끝
+
+
+// 별(5);
+
+function 별1 (n){
+
+    
+    for(let i=0; i<n; i++){
+        let answer = '';
+        for(let j=0; j<=i; j++){
+            answer += '*';
+        }
+        console.log( '이건뭐? '+ answer);
     }
 
-    return answer;
+}
 
-};// 함수 끝
-
-
-별(10);
+별1(5); 
