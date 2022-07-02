86d1808312ff43d1e85bcf4e3bd5b1bdb84114b9
seongjin77 <kssj9056@gmail.com>
2022-07-02 12:54:46 +0900

    노마드 코더

--- 노마더코더/2장.js
+++ 노마더코더/2장.js
@@ -1,8 +1,30 @@
-const cal = {
-    plus: function(a,b){
-        console.log(a+b);
-    }
+const title = document.querySelector('.hello h1');
+title.innerText='Hello!'
+
+
+function hand(){
+    title.style.color = 'blue';
+};
+
+function mouse(){
+    console.log('mouse is here')
 }
+function windowhandle(){
+    document.body.style.backgroundColor = 'tomato';
+}
+function copy(){
+    alert('copy');
+}
+function offline(){
+    alert('offline');
+}
+function online(){
+    alert('online');
+}
+title.addEventListener('click',hand );
+title.addEventListener('mouseenter',mouse );
 
-
-const cal1 = cal.plus(5,5);
+window.addEventListener('resize',windowhandle );
+window.addEventListener('copy',copy );
+window.addEventListener('offline',offline );
+window.addEventListener('online',online );
