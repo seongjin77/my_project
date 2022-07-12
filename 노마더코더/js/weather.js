d37ccad305f50349ee7b5a32edf533ca32740c7f
seongjin77 <kssj9056@gmail.com>
2022-07-12 23:32:08 +0900

    자바스크립트 딥 다이브

--- 노마더코더/js/weather.js
+++ 노마더코더/js/weather.js
@@ -1,10 +1,9 @@
-function onGeoOk (){
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-
-};
-function onGeoError (){
-    alert("can't find you");
+function onGeoOk() {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+}
+function onGeoError() {
+  alert("can't find you");
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError)
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
